2d2ab2918ad464ad80963ff433df48203c0a256a
Tomas <ttomycastro@gmail.com>
2024-12-26 17:47:12 -0300

    fix

--- src/app/modules/main/header/header.component.ts
+++ src/app/modules/main/header/header.component.ts
@@ -5,6 +5,8 @@ import {Component, HostBinding, OnInit} from '@angular/core';
 import {UntypedFormGroup, UntypedFormControl} from '@angular/forms';
 import {Store} from '@ngrx/store';
 import {AppService} from '@services/app.service';
+import { PlanesService } from '@services/planes/planes.service';
+import { SuscriptoresService } from '@services/suscriptores/suscriptores.service';
 import {Observable} from 'rxjs';
 
 const BASE_CLASSES = 'main-header navbar navbar-expand';
@@ -17,13 +19,17 @@ export class HeaderComponent implements OnInit {
     @HostBinding('class') classes: string = BASE_CLASSES;
     public ui: Observable<UiState>;
     public searchForm: UntypedFormGroup;
+    planes: any;
+    public meetingUrl: string = '';
 
     constructor(
         private appService: AppService,
-        private store: Store<AppState>
+        private store: Store<AppState>,
+        private planesService: PlanesService
     ) {}
 
     ngOnInit() {
+        this.getPlan();
         this.ui = this.store.select('ui');
         this.ui.subscribe((state: UiState) => {
             this.classes = `${BASE_CLASSES} ${state.navbarVariant}`;
@@ -45,6 +51,18 @@ export class HeaderComponent implements OnInit {
     logout() {
         this.appService.logout();
     }
+
+    getPlan() {
+        this.planesService.miPlan().subscribe(resp => {
+          this.planes = resp;
+          
+          if (this.planes.reason === "Premium") {
+            this.meetingUrl = 'https://mpago.la/2yXGGwQ';
+        } else {
+            this.meetingUrl = 'https://mpago.la/2K9ksYT';
+        }
+        });    
+      }
 
     onToggleMenuSidebar() {
         this.store.dispatch(new ToggleSidebarMenu());
