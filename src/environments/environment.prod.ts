89cc31065a2779c0d64c303d726b94c7b498ee32
Ezequiel López <contacto@ezequiellopez.ar>
2025-04-03 15:52:19 -0300

    upgrade api endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     clarityId: 'pcy6xo2f2k',
-    uri_api: 'https://api-php.Finnovate.cl/api/v1/',
-    uri_api_v2: 'https://api-v2.Finnovate.cl/v1',
+    uri_api: 'https://api-php.te-apoyo.cl/api/v1/',
+    uri_api_v2: 'https://api-v2.te-apoyo.cl/v1',
     //added a test cmy
     api_key: 'TAPY-694c87e2-3626-4f69-a121-8f9b29328958'
 };
